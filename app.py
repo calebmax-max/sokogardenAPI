9f382816f703c4c3297a34454851388d9b131694
Your Name <you@example.com>
2026-02-20 09:58:02 +0300

    updates

--- app.py
+++ app.py
@@ -43,9 +43,39 @@ def signup():
 
 
         return jsonify({"message" : "User registered successfully"})
+    
 
 
+# below is the login/sign in route
+@app.route("/api/signin", methods = ["POST"])
+def signin():
+     if request.method =="POST":
+       #extract data from the form
+       email = request.form["email"]
+       password = request.form["password"]
+
+       print(email,password)
+    #    return jsonify({"message" : "Signin route accessed"})
+       connection = pymysql.connect(host="localhost", user="root", password="", database="sokogardenonline")
+        # create a cursor to execute the sql queries
+       cursor = connection.cursor(pymysql.cursors.DictCursor)
+       sql ="SELECT * FROM users WHERE email = %s AND password = %s"
+        # create a tuple that will hold all the data gottten from the form.
+       data = (email, password)
+       #by use of the cursor execute the sql as you replace the placeholders with the actual values
+       cursor.execute(sql, data)
+       # check whether there are rows returned and store the same on a variable
+       count = cursor.rowcount
+        # if there are records it means the email and the password are coreect otherwise it means they are wrong
+
+       if count == 0:
+           return jsonify({"message" : "Login failed"})
+       else:
+        # There must be a user so we create a variable that will hold the details of the user fetched from the database
+           user = cursor.fetchone()
+        #return the deatils to the front-end as welll as a message
+           return jsonify({"message" : "User signed up successfully","user":user})
 
 
-# Run the application
+# Run the application  
 app.run(debug=True)
